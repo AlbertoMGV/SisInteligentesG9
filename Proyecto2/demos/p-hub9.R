9200b172a1b05c93d437e01c25e0c1cb63cd24a1
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-04-04 15:22:10 +0200

    init P2

---
+++ Proyecto2/demos/p-hub9.R
@@ -0,0 +1,52 @@
+# =======================================================================
+# Names: Alberto Miranda & Danel Lorente
+# Group Number: 
+# Assignment: Sistemas Inteligentes
+# Date: 2/03/2019
+# =======================================================================
+# 1. Be sure to include, with this template, any necessary files
+#    for execution, including datasets (problem.R, methodXXX.R, ...)
+#    (submission of the entire template folder is recommended)
+# 2. If you use a function of a certain package, do not forget to include the
+#    corresponding call to the "library ()" function
+# 3. Do not forget to comment on the code, especially those non-trivial commands
+#    (remember that part of the rating depends on the cleaning of the code)
+# 4. It is strongly recommended to test any implemented function in order to 
+#    check for its proper operation
+# =======================================================================
+# (This is a general code, you must adapt it)
+# =======================================================================
+# Configuring the Environment
+rm(list=ls())
+cat("\014")
+graphics.off()
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+dir()
+
+# LIBRARIES (add any needed library here)
+library(rstudioapi)
+library(ggplot2)
+library(gridExtra)
+
+# ADDITIONAL FUNCTIONS (add any used method/problem here)
+source("../problems/problem template.R")
+source("../methods/Breadth First Search.R")
+source("../methods/Depth First Search.R")
+
+# And here, there are additional (needed) functions
+source("../methods/Expand Node.R")
+source("../methods/Analyze Results.R")
+source("../methods/Plot Results.R")
+# =======================================================================
+# Check the proper operation of implemented function here!
+
+
+
+# =======================================================================
+# Solving of the problem (you have to adapt it)
+problem   = initialize.problem()
+res1 = Breadth.First.Search(problem, graph.search = T)
+res2 = Depth.First.Search(problem, graph.search = T)
+all = list(res1, res2)
+analyze.results(list(res1,res2),problem)
