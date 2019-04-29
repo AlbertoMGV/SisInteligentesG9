d0c552ea135f3e87a5da5044a4bc2a773a4719da
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-04-29 13:12:27 +0200

    Data added + Panel edited

--- Proyecto3/app.R
+++ Proyecto3/app.R
@@ -11,35 +11,108 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Data Analyzer"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
+   navlistPanel(
+     "Menu",
+     tabPanel("Subir Fichero",
+              # Sidebar panel for inputs ----
+                
+                  
+                  # Input: Select a file ----
+                  fileInput("file1", "Choose CSV File",
+                            multiple = FALSE,
+                            accept = c("text/csv",
+                                       "text/comma-separated-values,text/plain",
+                                       ".csv")),
+                  
+                  # Horizontal line ----
+                  tags$hr(),
+                  
+                  # Input: Checkbox if file has header ----
+                  checkboxInput("header", "Header", TRUE),
+                  
+                  # Input: Select separator ----
+                  radioButtons("sep", "Separator",
+                               choices = c(Comma = ",",
+                                           Semicolon = ";",
+                                           Tab = "\t"),
+                               selected = ","),
+                  
+                  # Input: Select quotes ----
+                  radioButtons("quote", "Quote",
+                               choices = c(None = "",
+                                           "Double Quote" = '"',
+                                           "Single Quote" = "'"),
+                               selected = '"'),
+                  
+                  # Horizontal line ----
+                  tags$hr(),
+                  
+                  # Input: Select number of rows to display ----
+                  radioButtons("disp", "Display",
+                               choices = c(Head = "head",
+                                           All = "all"),
+                               selected = "head")
+                
+              ),
+     tabPanel("Analisis de Datos",
+                # Sidebar with a slider input for number of bins 
+                sidebarLayout(
+                  sidebarPanel(
+                    sliderInput("bins",
+                                "Number of bins:",
+                                min = 1,
+                                max = 50,
+                                value = 30)
+                  ),
+                  
+                  # Show a plot of the generated distribution
+                  mainPanel(
+                    plotOutput("contents")
+                  )
+                )
+              )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$contents <- renderTable({
+     
+     # input$file1 will be NULL initially. After the user selects
+     # and uploads a file, head of that data file by default,
+     # or all rows if selected, will be shown.
+     
+     req(input$file1)
+     
+     # when reading semicolon separated files,
+     # having a comma separator causes `read.csv` to error
+     tryCatch(
+       {
+         df <- read.csv(input$file1$datapath,
+                        header = input$header,
+                        sep = input$sep,
+                        quote = input$quote)
+         print(df)
+       },
+       error = function(e) {
+         # return a safeError if a parsing error occurs
+         print("#################")
+         print("Algo ha petado xd")
+         stop(safeError(e))
+       }
+     )
+     
+     if(input$disp == "head") {
+       
+       return(head(df))
+     }
+     else {
+       return(df)
+     }
+     
    })
 }
 
