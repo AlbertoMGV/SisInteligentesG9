6d2351d68a0b9de22a7ddbb92cce6d7281b220f8
Danelue <Dnl@users.noreply.github.com>
2019-05-06 12:26:48 +0200

    Proyecto3Danel

---
+++ Proyecto3Danel/app/app/server.R
@@ -0,0 +1,48 @@
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$target <- renderTable({
+    
+  })
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        dat <- read.keel(input$file1$datapath)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+    if(input$disp == "head") {
+      return(head(dat))
+    }
+    else {
+      return(dat)
+    }
+    
+    
+  })
+  
+  
+  
+  
+}
---
+++ Proyecto3Danel/app/app/ui.R
@@ -0,0 +1,84 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+rm(list=ls())
+cat("\014")
+graphics.off()
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+dir()
+
+
+library(shiny)
+library(RKEEL)
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  titlePanel("Upload a File & Select attribute"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select a file ----
+      fileInput("file1", "1. Choose a File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv",".dat")),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Header", TRUE),
+      
+      # Input: Select separator ----
+      radioButtons("sep", "2. Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      
+      # Input: Select quotes ----
+      radioButtons("quote", "3. Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'),
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Select number of rows to display ----
+      radioButtons("disp", "4. Display",
+                   choices = c(Head = "head",
+                               All = "all"),
+                   selected = "head"),
+      selectInput("combobox","5. Select an attribute to predict",choices = c("Batting_average" = 1,"On.base_percentage"=2,
+                                                                             "Runs"=3,"Hits"=4,"Doubles"=5,"Triples"=6,"HomeRuns"=7,"Runs_batted_in"=8,"Walks"=9,"Strike.Outs"=10,
+                                                                             "Stolen_bases"=11,"Errors"=12,"Free_agency_eligibility"=13,"Free_agent"=14,"Arbitration_eligibility"=15,"Arbitration"=16,
+                                                                             "Salary"=17))
+      
+      
+    ),
+    
+    
+    mainPanel(
+      tabsetPanel(id = "tabs",
+                  tabPanel("Content",tableOutput("contents")),
+                  tabPanel("Target",tableOutput("target"))
+                  
+      )
+      
+    )
+  )
+  
+)
+
