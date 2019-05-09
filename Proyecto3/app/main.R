e0fbce462a5268ffa86fbf2dc3095f2712904a3b
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-05-09 19:43:25 +0200

    Training.data changed

--- Proyecto3/app/main.R
+++ Proyecto3/app/main.R
@@ -21,12 +21,14 @@ library(lattice)
 data = read.keel("../data/baseball.dat")
 
 # Split the data into training and test set
-training.samples <- data$target %>% createDataPartition(p = 0.8, list = FALSE)
+training.samples <- createMultiFolds(y=data$Salary,k = 10, times = 5)
 
-train.data  <- data[training.samples, ]
-test.data <- data[-training.samples, ]
+
+
+train.data  <- data[training.samples[[i]], ]
+test.data <- data[-training.samples[[i]], ]
 # CREO MODELO LINEAL
-linear.model <- lm(target ~., data = train.data)
+linear.model <- lm(target ~., train.data)
 #PREDICCIONES
 predictions <- linear.model %>% predict(test.data)
 #ERROR
@@ -35,5 +37,14 @@ abs(predictions - test.data$target)
 data.frame( R2 = R2(predictions, test.data$target),
             RMSE = RMSE(predictions, test.data$target),
             MAE = MAE(predictions, test.data$target))
+
+# Define training control
+train.control <- trainControl(method = "cv", number = 5)
+# Train the model
+model <- train(Fertility ~., data, method = "lm",
+               trControl = train.control)
+# Summarize the results
+
+#???print(model)
 
 print(data)
