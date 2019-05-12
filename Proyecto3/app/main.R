068866bd1fd395b18943bd05143f7fbcd50a1993
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-05-12 21:45:52 +0200

    Accuracy NAN

--- Proyecto3/app/main.R
+++ Proyecto3/app/main.R
@@ -19,33 +19,40 @@ library(ggplot2)
 library(lattice)
 
 data = read.keel("../data/baseball.dat")
+
   
 # Split the data into training and test set
-folds <- createMultiFolds(y=data$Salary,k = 5, times = 1)
 
-print(folds)
+folds <- createMultiFolds(y = data$Salary, k = 10, times = 5)
 
 for(i in 1:5){
+  
   #Segement your data by fold using the which() function 
   train.data  <- data[folds[[i]], ]
   test.data <- data[-folds[[i]], ]
+  
   #Use the test and train data partitions however you desire...
   
-  linear.model <- lm(Salary~., train.data)
+  linear.model <- lm(Salary ~., train.data)
   prediction <- predict(linear.model, test.data)
-  abs(prediction - test.data$Salary)
   
-  data.frame( R2 = R2(prediction, test.data$Salary),
+  accuracy <- data.frame( R2 = R2(prediction, test.data$Salary, form = "traditional"),
               RMSE = RMSE(prediction, test.data$Salary),
               MAE = MAE(prediction, test.data$Salary))
   
-  # Define training control
-  train.control <- trainControl(method = "cv", number = 5)
-  # Train the model
-  model <- train(Salary~., data, method = "lm",
-                 trControl = train.control)
-
-  print(model)
+  print(accuracy)
+  
   
 }
 
+# Define training control
+#train.control <- trainControl(method = "cv", number = 5, savePredictions = "all")
+# Train the model
+#model <- train(Salary ~., data = data$Salary, method = "lm",
+#trControl = train.control)
+
+#summary(model)
+
+#model$result
+
+#print(prediction - test.data)
