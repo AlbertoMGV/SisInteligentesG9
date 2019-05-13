61db48cc04e531a75fd970700a707128460181b4
Danelue <Dnl@users.noreply.github.com>
2019-05-13 10:09:11 +0200

    Cambio a numerico
    cambiado la columna de salario a numerico

--- Proyecto3/app/main.R
+++ Proyecto3/app/main.R
@@ -1,6 +1,6 @@
 # =======================================================================
 # Names: Alberto Miranda & Danel Lorente
-# Group Number: 
+# Group Number:
 # Assignment: Sistemas Inteligentes
 # Date: 23/04/2019
 # =======================================================================
@@ -19,30 +19,30 @@ library(ggplot2)
 library(lattice)
 
 data = read.keel("../data/baseball.dat")
+data$Salary=as.numeric(data$Salary)
 
-  
 # Split the data into training and test set
 
 folds <- createMultiFolds(y = data$Salary, k = 10, times = 5)
-
+print(folds)
 for(i in 1:5){
-  
-  #Segement your data by fold using the which() function 
+
+  #Segement your data by fold using the which() function
   train.data  <- data[folds[[i]], ]
   test.data <- data[-folds[[i]], ]
-  
+
   #Use the test and train data partitions however you desire...
-  
+
   linear.model <- lm(Salary ~., train.data)
   prediction <- predict(linear.model, test.data)
-  
-  accuracy <- data.frame( R2 = R2(prediction, test.data$Salary, form = "traditional"),
-              RMSE = RMSE(prediction, test.data$Salary),
-              MAE = MAE(prediction, test.data$Salary))
-  
+
+  accuracy <-  data.frame( R2 = R2(prediction, test.data$Salary, form = "traditional"),
+                           RMSE = RMSE(prediction, test.data$Salary),
+                           MAE = MAE(prediction, test.data$Salary))
+
   print(accuracy)
-  
-  
+
+
 }
 
 # Define training control
--- Proyecto3/app/server.R
+++ Proyecto3/app/server.R
@@ -1,25 +1,25 @@
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  output$target <- renderTable({
-    
+
+  output$target <- renderPlot({
+
   })
   output$contents <- renderTable({
-    
+
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
     # or all rows if selected, will be shown.
-    
+
     req(input$file1)
-    
+
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
     tryCatch(
@@ -31,18 +31,18 @@ server <- function(input, output) {
         stop(safeError(e))
       }
     )
-    
+
     if(input$disp == "head") {
       return(head(dat))
     }
     else {
       return(dat)
     }
-    
-    
+
+
   })
-  
-  
-  
-  
+
+
+
+
 }
