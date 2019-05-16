cc5a850610ee6cf0d67b2242682826e3c30e945a
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-05-16 22:32:01 +0200

    Code part finished

--- Proyecto4/app/app.R
+++ Proyecto4/app/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Upload a File & Select attribute"),
+   titlePanel("Proyecto 4: Arboles de Decision"),
    
    # Sidebar layout with input and output definitions ----
    sidebarLayout(
@@ -44,8 +44,11 @@ ui <- fluidPage(
                    max = 30,
                    value = 30),
        
-       selectInput("combobox","CP:",choices = c(0,1))
-       
+       sliderInput(inputId = "cp",
+                   label = "CP:",
+                   min = 0,
+                   max = 1,
+                   value = 0.5)
        
      ),
      
@@ -53,7 +56,8 @@ ui <- fluidPage(
      mainPanel(
        tabsetPanel(id = "tabs",
                    tabPanel("Contenido",tableOutput("contents")),
-                   tabPanel("% Aciertos",plotOutput("target"))
+                   tabPanel("% Aciertos",plotOutput("target")),
+                   tabPanel("Arbol",plotOutput("arbol"))
        )
        
      )
@@ -85,8 +89,10 @@ server <- function(input, output) {
       train.data  <- data[folds[[i]], ]
       test.data <- data[-folds[[i]], ]
       #Arbol de decis por cada uno
-      #TODO: Aqui tenemos que meter el min max y CP!!!
-      model <- rpart(formula=Churn~., data=train.data)
+      parametros <- rpart.control(minsplit = input$minsplit,
+                                  maxdepth = input$maxdepth,
+                                  cp = input$cp)
+      model <- rpart(formula=Churn~., data=train.data, method = "class", control = parametros)
       #Predicciones
       prediction <- predict(model, test.data, type="class")
       #acciertos
@@ -99,20 +105,43 @@ server <- function(input, output) {
     barplot(porcenAciertos,main="Tasa de aciertos por ejecucion", xlab=paste("Media de la tasa de aciertos (linea roja): ", toString(round(tasaAciertoITotal/5,5)),"%"), col=c("lightblue"), ylim=c(0,1),names.arg=c(paste(round(porcenAciertos[1],5),"%"),paste(round(porcenAciertos[2],5),"%"), paste(round(porcenAciertos[3],5),"%"), paste(round(porcenAciertos[4],5),"%"), paste(round(porcenAciertos[5],5),"%")))
     abline(h=tasaAciertoITotal/5, col="red")
     
-    rpart.plot(x=model)
+  })
+  output$arbol <- renderPlot({
+    
+    #Cargamos data
+    
+    data <- read.csv(file = input$file1$datapath)
+    folds <- createMultiFolds(y = data$Churn, k = 10, times = 5)
+    tasaAciertoIndividual = 0
+    tasaAciertoITotal = 0
+    porcenAciertos = c()
+    
+    for(i in 1:5){
+      tasaAciertoIndividual = 0
+      #Creamos sets de datos
+      train.data  <- data[folds[[i]], ]
+      test.data <- data[-folds[[i]], ]
+      #Arbol de decis por cada uno
+      parametros <- rpart.control(minsplit = input$minsplit,
+                               maxdepth = input$maxdepth,
+                               cp = input$cp)
+      model <- rpart(formula=Churn~., data=train.data, method = "class", control = parametros)
+      #Predicciones
+      prediction <- predict(model, test.data, type="class")
+      #acciertos
+      tasaAciertoIndividual = sum(prediction == test.data$Churn) / nrow(test.data)
+      tasaAciertoITotal = tasaAciertoITotal + tasaAciertoIndividual
+      porcenAciertos = c(porcenAciertos,tasaAciertoIndividual)
+      
+    }
+    
+    rpart.plot(x=model, main="Representacion del Arbol de decision")
     
     
   })
   output$contents <- renderTable({
     
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
     req(input$file1)
-    
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
     tryCatch(
       {
         #Leemos el archivo
