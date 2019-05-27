9a0028872d1da6e96d154b458577f97fc34df665
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-05-27 21:52:13 +0200

    Objeto pelicula & Main created

--- Proyecto5/src/main/java/com/albertomgv/proyecto5/MetodoPrincipal.java
+++ Proyecto5/src/main/java/com/albertomgv/proyecto5/MetodoPrincipal.java
@@ -1,9 +1,102 @@
 package com.albertomgv.proyecto5;
 
+import org.kie.api.KieServices;
+import org.kie.api.logger.KieRuntimeLogger;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import com.albertomgv.proyecto5.Pelicula.Apologia;
+import com.albertomgv.proyecto5.Pelicula.Categoria;
+import com.albertomgv.proyecto5.Pelicula.Conductas;
+import com.albertomgv.proyecto5.Pelicula.Contenido;
+import com.albertomgv.proyecto5.Pelicula.Escenas;
+import com.albertomgv.proyecto5.Pelicula.Lenguaje;
+import com.albertomgv.proyecto5.Pelicula.Practicas_Discri;
+import com.albertomgv.proyecto5.Pelicula.Valores;
+
 public class MetodoPrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Creamos las peliculas con diferentes detalles
+		
+		Pelicula p1 = new Pelicula();
+		p1.setName("Tetanic");
+		p1.setEscena(Escenas.DESNUDEZ);
+		p1.setCategoria(Categoria.PORNOGRAFIA);
+		p1.setValores(Valores.ESTIMULA_IMAGINACION);
+		
+		Pelicula p2 = new Pelicula();
+		p2.setName("Tetanic");
+		p2.setEscena(Escenas.DESNUDEZ);
+		p2.setCategoria(Categoria.PORNOGRAFIA);
+		p2.setValores(Valores.ESTIMULA_IMAGINACION);
+		
+		Pelicula p3 = new Pelicula();
+		p3.setName("Tetanic");
+		p3.setEscena(Escenas.DESNUDEZ);
+		p3.setCategoria(Categoria.PORNOGRAFIA);
+		p3.setValores(Valores.ESTIMULA_IMAGINACION);
+		
+		Pelicula p4 = new Pelicula();
+		p4.setName("Tetanic");
+		p4.setEscena(Escenas.DESNUDEZ);
+		p4.setCategoria(Categoria.PORNOGRAFIA);
+		p4.setValores(Valores.ESTIMULA_IMAGINACION);
+		
+		Pelicula p5 = new Pelicula();
+		p5.setName("Tetanic");
+		p5.setEscena(Escenas.DESNUDEZ);
+		p5.setCategoria(Categoria.PORNOGRAFIA);
+		p5.setValores(Valores.ESTIMULA_IMAGINACION);
+		
+		//Mostramos la info de las peliculas antes del metodo
+		
+		System.out.println("ANTES DEL PROCESO DE INFERENCIA");
+		System.out.println("----- --- ------- -- ----------");
+		System.out.println(p1);
+		System.out.println("\n" + p2);
+		System.out.println("\n" + p3);
+		System.out.println("\n" + p4);
+		System.out.println("\n" + p5);
+		
+        // Instanciamos la factoria de kie
+        KieServices kServices = KieServices.Factory.get();
+        
+        // Para almacenar el conocimiento
+        KieContainer kContainer = kServices.getKieClasspathContainer();
+        
+        // Se crea una sesión KIE en el contenedor a partir del fichero de configuración  
+        // META-INF/kmodule.xml.
+        KieSession kSession = kContainer.newKieSession("peliculas_run");
+        
+        // Se configura el log del proceso de inferencia
+        KieRuntimeLogger logger = kServices.getLoggers().newFileLogger(kSession, "./log/peliculas");
+				
+		//Se insertan los objetos en la KIE Session (Memoria de trabajo)
+		kSession.insert(p1);
+		kSession.insert(p2);
+		kSession.insert(p3);
+		kSession.insert(p4);
+		kSession.insert(p5);
+		
+		System.out.println("\n\nDURANTE EL PROCESO DE INFERENCIA");
+		System.out.println("------- -- ------- -- ----------");
+
+		//Se invoca al proceso de inferencia
+		kSession.fireAllRules();
+		
+		//Se cierra el log
+		logger.close();
+
+		//Se muestra el contenido de los objetos depues del proceso de inferencia
+		System.out.println("\n\nDESPUES DEL PROCESO DE INFERENCIA");
+		System.out.println("------- --- ------- -- ----------");
+		System.out.println(p1);
+		System.out.println("\n" + p2);
+		System.out.println("\n" + p3);
+		System.out.println("\n" + p4);
+		System.out.println("\n" + p5);		
 
 	}
 
--- Proyecto5/src/main/java/com/albertomgv/proyecto5/Pelicula.java
+++ Proyecto5/src/main/java/com/albertomgv/proyecto5/Pelicula.java
@@ -1,5 +1,194 @@
 package com.albertomgv.proyecto5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pelicula {
+	
+	public enum Conductas {		
+		ANTISOCIALES,
+		INCIVICAS,
+		ILEGALES,
+		PERJU_DESARRO_PSICO,
+		GRATE_ABUSO
+	}
+	
+	public enum Practicas_Discri {		
+		GENERO,
+		ETNICO,
+		SEXUAL
+	}
+	
+	public enum Escenas {		
+		VIOLENCIA_VERBAL,
+		VIOLENCIA_FISICA,
+		ANSIEDAD,
+		MIEDO,
+		DOLOR,
+		TENSION_EMOCIONAL,
+		DESNUDEZ,
+		DIALOG_EROTICOS,
+		CRUELDAD,
+		INQUIETUD
+	}
+	
+	public enum Lenguaje {		
+		HIRIENTE,
+		AGRADABLE
+	}
+	
+	public enum Contenido {		
+		FACIL_COMPRENSIBLE,
+		PERJU_DESARR_PSICO,
+		HABITUAL_ADULTOS
+	}
+	
+	public enum Valores {		
+		PEDAGOGICOS,
+		SOCIALES,
+		ESTIMULA_IMAGINACION,
+		PROMUEVE_REFLEXION
+	}
+	
+	public enum Apologia {		
+		DROGAS,
+		VIOLENCIA,
+		CONDUCTAS_ANTISOCIALES,
+		PRACTICAS_DISCRIMINATORIAS		
+	}
+	
+	public enum Categoria {		
+		INFANTIL,
+		PORNOGRAFIA,
+		ADULTOS
+	}
+	
+	private String name = null;
+	private Conductas conducta=null;
+	private Practicas_Discri practica=null;
+	private Escenas escena=null;
+	private Lenguaje lenguaje=null;
+	private Contenido cont=null;
+	private Valores valores=null;
+	private Apologia apologia=null;
+	private Categoria categoria=null;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the conducta
+	 */
+	public Conductas getConducta() {
+		return conducta;
+	}
+	/**
+	 * @param conducta the conducta to set
+	 */
+	public void setConducta(Conductas conducta) {
+		this.conducta = conducta;
+	}
+	/**
+	 * @return the practica
+	 */
+	public Practicas_Discri getPractica() {
+		return practica;
+	}
+	/**
+	 * @param practica the practica to set
+	 */
+	public void setPractica(Practicas_Discri practica) {
+		this.practica = practica;
+	}
+	/**
+	 * @return the escena
+	 */
+	public Escenas getEscena() {
+		return escena;
+	}
+	/**
+	 * @param escena the escena to set
+	 */
+	public void setEscena(Escenas escena) {
+		this.escena = escena;
+	}
+	/**
+	 * @return the lenguaje
+	 */
+	public Lenguaje getLenguaje() {
+		return lenguaje;
+	}
+	/**
+	 * @param lenguaje the lenguaje to set
+	 */
+	public void setLenguaje(Lenguaje lenguaje) {
+		this.lenguaje = lenguaje;
+	}
+	/**
+	 * @return the cont
+	 */
+	public Contenido getCont() {
+		return cont;
+	}
+	/**
+	 * @param cont the cont to set
+	 */
+	public void setCont(Contenido cont) {
+		this.cont = cont;
+	}
+	/**
+	 * @return the valores
+	 */
+	public Valores getValores() {
+		return valores;
+	}
+	/**
+	 * @param valores the valores to set
+	 */
+	public void setValores(Valores valores) {
+		this.valores = valores;
+	}
+	/**
+	 * @return the apologia
+	 */
+	public Apologia getApologia() {
+		return apologia;
+	}
+	/**
+	 * @param apologia the apologia to set
+	 */
+	public void setApologia(Apologia apologia) {
+		this.apologia = apologia;
+	}
+	/**
+	 * @return the categoria
+	 */
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	@Override
+	public String toString() {
+		return "Pelicula [conducta=" + conducta + ", practica=" + practica + ", escena=" + escena + ", lenguaje="
+				+ lenguaje + ", cont=" + cont + ", valores=" + valores + ", apologia=" + apologia + ", categoria="
+				+ categoria + "]";
+	}
+	
+	
 
 }
